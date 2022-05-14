57709b492adedec44b97835bda254064051a381e
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-14 17:23:15 +0800

    date

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,25 @@
     <div class="m-auto">
       <h1 class="text-5xl">World Clock</h1>
       <p class="mt-5 text-xl">international dates and times</p>
-      <div class="grid grid-cols-2 gap-5 m-5 text-white">
-        <button class="bg-black px-5 py-3">Start timer</button>
-        <button class="bg-black px-5 py-3">Reset timer</button>
+      <div id="malaysia">
+        <p>Current date and time is {{ date }}.</p>
+      </div>
+      <div id="timer">
+        <div class="grid grid-cols-2 gap-5 m-5 text-white">
+          <button class="bg-black px-5 py-3">Start timer</button>
+          <button class="bg-black px-5 py-3">Reset timer</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: new Date(),
+    }
+  }
+}
+</script>

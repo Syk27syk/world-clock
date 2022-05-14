325c428e208f3f46b74d8bd711bd20bb113f2d16
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-14 17:01:06 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl">World Clock</h1>
+      <p class="mt-5 text-xl">international dates and times</p>
+      <div class="grid grid-cols-2 gap-5 m-5 text-white">
+        <button class="bg-black px-5 py-3">Start timer</button>
+        <button class="bg-black px-5 py-3">Reset timer</button>
+      </div>
     </div>
   </div>
 </template>
